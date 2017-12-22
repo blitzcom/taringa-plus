4870bc5fcc627503affcab1b7cdeb50018994fe1
Diego Lopez Garcia <aud0l@hotmail.com>
2017-12-21 22:30:45 -0600

    Agregar Semantic UI
    - Agregar Semantic UI para react y el framework css.
    
    - Eliminar los archivos y código por defecto de create-react-app.
    
    - Agregar component Nav.

---
+++ src/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Menu, Container } from 'semantic-ui-react'
+
+const Nav = (props) => {
+  return (
+    <Menu fixed='top' size='large' inverted>
+      <Container>
+        <Menu.Item header>Taringa Plus!</Menu.Item>
+      </Container>
+    </Menu>
+  )
+}
+
+export default Nav
