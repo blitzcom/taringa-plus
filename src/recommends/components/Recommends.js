a650e4ded049e975cbd032817e704e466adcbe7b
Diego Lopez Garcia <aud0l@hotmail.com>
2018-01-01 18:34:01 -0600

    Cambios menores

--- src/recommends/components/Recommends.js
+++ src/recommends/components/Recommends.js
@@ -1,36 +1,31 @@
 import _ from 'lodash'
-import React, { Component } from 'react'
+import React from 'react'
 import { Item } from 'semantic-ui-react'
 
-import * as actions from '../actions'
 import Recommend from './Recommend'
 
-export class Recommends extends Component {
-  render () {
-    const { status, posts } = this.props
-
-    if (status === 'fetching') {
-      return (
-        <Item.Group>
-          {
-            _.times(20, (i) => ({ id: i})).map(item => (
-              <Recommend key={item.id} placeholder/>
-            ))
-          }
-        </Item.Group>
-      )
-    }
-
+export const Recommends = ({ status, posts }) => {
+  if (status === 'fetching') {
     return (
       <Item.Group>
         {
-          posts.map(post => (
-            <Recommend key={post.id} {...post}/>
+          _.times(20, (i) => ({ id: i})).map(item => (
+            <Recommend key={item.id} placeholder/>
           ))
         }
       </Item.Group>
     )
   }
+
+  return (
+    <Item.Group>
+      {
+        posts.map(post => (
+          <Recommend key={post.id} {...post}/>
+        ))
+      }
+    </Item.Group>
+  )
 }
 
 Recommends.defaultProps = {
